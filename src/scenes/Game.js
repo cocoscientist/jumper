3177c3234c98526e098f1a1c5b22e3f5dd22137c
Yash <ypt7895@gmail.com>
2023-06-24 21:03:18 +0530

    added player controls

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -9,6 +9,7 @@ export default class Game extends Phaser.Scene{
         this.load.image('background','assets/bg_layer1.png')
         this.load.image('platform','assets/ground_grass.png')
         this.load.image('bunny-stand','assets/bunny1_stand.png')
+        this.cursors = this.input.keyboard.createCursorKeys()
     }
 
     create(){
@@ -36,9 +37,10 @@ export default class Game extends Phaser.Scene{
         this.player.body.checkCollision.right = false
 
         this.cameras.main.startFollow(this.player)
+        this.cameras.main.setDeadzone(this.scale.width*1.5)
     }
 
-    update(){
+    update(t, dt){
         this.platforms.children.iterate(child=>{
             const scrollY = this.cameras.main.scrollY
             if (child.y >= scrollY + 700){
@@ -50,6 +52,14 @@ export default class Game extends Phaser.Scene{
         const touchingDown = this.player.body.touching.down
         if(touchingDown){
             this.player.setVelocityY(-300)
+        }
+
+        if(this.cursors.left.isDown && !touchingDown){
+            this.player.setVelocityX(-200)
+        }else if(this.cursors.right.isDown && !touchingDown){
+            this.player.setVelocityX(200)
+        }else{
+            this.player.setVelocityX(0)
         }
     }
 }
