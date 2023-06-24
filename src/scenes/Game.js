3a604ab33dd4285df04ad63dd5b280d8dd9268f0
Yash <ypt7895@gmail.com>
2023-06-24 20:49:31 +0530

    infinite platforms, collisions for player

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -12,16 +12,16 @@ export default class Game extends Phaser.Scene{
     }
 
     create(){
-        this.add.image(240,320,'background')
+        this.add.image(240,320,'background').setScrollFactor(1,0)
         
-        const platforms = this.physics.add.staticGroup()
+        this.platforms = this.physics.add.staticGroup()
 
         for(let i=0;i<5;i++){
             const x = Phaser.Math.Between(80,400)
             const y = 150*i
 
             /** @type {Phaser.Physics.Arcade.Sprite} */
-            const platform = platforms.create(x,y,'platform')
+            const platform = this.platforms.create(x,y,'platform')
             platform.scale = 0.5
 
             /** @type {Phaser.Physics.Arcade.StaticBody} */
@@ -29,7 +29,27 @@ export default class Game extends Phaser.Scene{
             body.updateFromGameObject()
         }
 
-        const player = this.physics.add.sprite(240,320,'bunny-stand').setScale(0.5)
-        this.physics.add.collider(platforms,player)
+        this.player = this.physics.add.sprite(240,320,'bunny-stand').setScale(0.5)
+        this.physics.add.collider(this.platforms,this.player)
+        this.player.body.checkCollision.up = false
+        this.player.body.checkCollision.left = false
+        this.player.body.checkCollision.right = false
+
+        this.cameras.main.startFollow(this.player)
+    }
+
+    update(){
+        this.platforms.children.iterate(child=>{
+            const scrollY = this.cameras.main.scrollY
+            if (child.y >= scrollY + 700){
+                child.y = scrollY - Phaser.Math.Between(50, 100)
+                child.body.updateFromGameObject()
+            }
+        })
+
+        const touchingDown = this.player.body.touching.down
+        if(touchingDown){
+            this.player.setVelocityY(-300)
+        }
     }
 }
